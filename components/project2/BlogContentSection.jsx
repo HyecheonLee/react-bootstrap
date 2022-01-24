ddc8508adc645e72349dec088a7fc47a8803d562
이혜천 <rainbow880616@gmail.com>
2022-01-24 22:21:13 +0900

    BlogTemplate

---
+++ components/project2/BlogContentSection.jsx
@@ -0,0 +1,203 @@
+import React from 'react';
+
+const BlogContentSection = () => {
+  return (
+    <main id="blogContent">
+      <div className={"section-content"}>
+        <div className="container-xxl">
+          <div className={"row row-cols-1 row-cols-sm-2 row-cols-md-3 gx-3 gy-2 grid"}>
+            <div className={"col grid-item"}>
+              <div className={"card mb-3"}>
+                <div className={"row g-0"}>
+                  <div className={"col-4 post-featured"}>
+                    <img src="/project2/images/port1.jpg"/>
+                  </div>
+                  <div className="col-8 post-text d-flex flex-column align-items-stretch">
+                    <div className={"card-body"}>
+                      <h5 className={"card-title mb-3"}>
+                        <a href={"/project/post"}
+                           className={"text-decoration-none mb-3"}>Post title</a>
+                      </h5>
+                      <p className={"card-text fw-light text-truncate"}>Lorem ipsum
+                        dolor sit amet,
+                        consectetur adipisicing elit. Maiores, modi.</p>
+                    </div>
+                    <div className={"card-footer border-0"}>
+                      <small className={"text-muted fw-light"}>
+                        <img src="/project2/images/avatar.png" alt={"Avatar"}
+                             className={"img-avatar"}/>CodingDIY | 2022-01-24 |
+                        Category1
+                      </small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={"col grid-item"}>
+              <div className={"card mb-3"}>
+                <div className={"row g-0"}>
+                  <div className={"col-4 post-featured"}>
+                    <img src="/project2/images/port2.jpg"/>
+                  </div>
+                  <div className="col-8 post-text d-flex flex-column align-items-stretch">
+                    <div className={"card-body"}>
+                      <h5 className={"card-title mb-3"}>
+                        <a href={"/project/post"}
+                           className={"text-decoration-none mb-3"}>Post title</a>
+                      </h5>
+                      <p className={"card-text fw-light text-truncate"}>Lorem ipsum
+                        dolor sit amet,
+                        consectetur adipisicing elit. Maiores, modi.</p>
+                    </div>
+                    <div className={"card-footer border-0"}>
+                      <small className={"text-muted fw-light"}>
+                        <img src="/project2/images/avatar.png" alt={"Avatar"}
+                             className={"img-avatar"}/>CodingDIY | 2022-01-24 |
+                        Category1
+                      </small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={"col grid-item"}>
+              <div className={"card mb-3"}>
+                <div className={"row g-0"}>
+                  <div className={"col-4 post-featured"}>
+                    <img src="/project2/images/port3.jpg"/>
+                  </div>
+                  <div className="col-8 post-text d-flex flex-column align-items-stretch">
+                    <div className={"card-body"}>
+                      <h5 className={"card-title mb-3"}>
+                        <a href={"/project/post"}
+                           className={"text-decoration-none mb-3"}>Post title</a>
+                      </h5>
+                      <p className={"card-text fw-light text-truncate"}>Lorem ipsum
+                        dolor sit amet,
+                        consectetur adipisicing elit. Maiores, modi.</p>
+                    </div>
+                    <div className={"card-footer border-0"}>
+                      <small className={"text-muted fw-light"}>
+                        <img src="/project2/images/avatar.png" alt={"Avatar"}
+                             className={"img-avatar"}/>CodingDIY | 2022-01-24 |
+                        Category1
+                      </small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={"col grid-item"}>
+              <div className={"card mb-3"}>
+                <div className={"row g-0"}>
+                  <div className={"col-4 post-featured"}>
+                    <img src="/project2/images/port4.jpg"/>
+                  </div>
+                  <div className="col-8 post-text d-flex flex-column align-items-stretch">
+                    <div className={"card-body"}>
+                      <h5 className={"card-title mb-3"}>
+                        <a href={"/project/post"}
+                           className={"text-decoration-none mb-3"}>Post title</a>
+                      </h5>
+                      <p className={"card-text fw-light text-truncate"}>Lorem ipsum
+                        dolor sit amet,
+                        consectetur adipisicing elit. Maiores, modi.</p>
+                    </div>
+                    <div className={"card-footer border-0"}>
+                      <small className={"text-muted fw-light"}>
+                        <img src="/project2/images/avatar.png" alt={"Avatar"}
+                             className={"img-avatar"}/>CodingDIY | 2022-01-24 |
+                        Category1
+                      </small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={"col grid-item"}>
+              <div className={"card mb-3"}>
+                <div className={"row g-0"}>
+                  <div className={"col-4 post-featured"}>
+                    <img src="/project2/images/port5.jpg"/>
+                  </div>
+                  <div className="col-8 post-text d-flex flex-column align-items-stretch">
+                    <div className={"card-body"}>
+                      <h5 className={"card-title mb-3"}>
+                        <a href={"/project/post"}
+                           className={"text-decoration-none mb-3"}>Post title</a>
+                      </h5>
+                      <p className={"card-text fw-light text-truncate"}>Lorem ipsum
+                        dolor sit amet,
+                        consectetur adipisicing elit. Maiores, modi.</p>
+                    </div>
+                    <div className={"card-footer border-0"}>
+                      <small className={"text-muted fw-light"}>
+                        <img src="/project2/images/avatar.png" alt={"Avatar"}
+                             className={"img-avatar"}/>CodingDIY | 2022-01-24 |
+                        Category1
+                      </small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={"col grid-item"}>
+              <div className={"card mb-3"}>
+                <div className={"row g-0"}>
+                  <div className={"col-4 post-featured"}>
+                    <img src="/project2/images/port6.jpg"/>
+                  </div>
+                  <div className="col-8 post-text d-flex flex-column align-items-stretch">
+                    <div className={"card-body"}>
+                      <h5 className={"card-title mb-3"}>
+                        <a href={"/project/post"}
+                           className={"text-decoration-none mb-3"}>Post title</a>
+                      </h5>
+                      <p className={"card-text fw-light text-truncate"}>Lorem ipsum
+                        dolor sit amet,
+                        consectetur adipisicing elit. Maiores, modi.</p>
+                    </div>
+                    <div className={"card-footer border-0"}>
+                      <small className={"text-muted fw-light"}>
+                        <img src="/project2/images/avatar.png" alt={"Avatar"}
+                             className={"img-avatar"}/>CodingDIY | 2022-01-24 |
+                        Category1
+                      </small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br/>
+          <div className={"row justify-content-center my-5"}>
+            <div className={"col-md-8"}>
+              <nav aria-label="Page navigation">
+                <ul className={"pagination justify-content-center"}>
+                  <li className={"page-item disabled"}>
+                    <a href={"#"} className={"page-link"} textindex="-1"
+                       aria-disabled="true">Previous</a>
+                  </li>
+                  <li className={"page-item"}>
+                    <a href={"#"} className={"page-link"}>1</a>
+                  </li>
+                  <li className={"page-item active"} aria-current="page">
+                    <a href={"#"} className={"page-link"}>2</a>
+                  </li>
+                  <li className={"page-item"}>
+                    <a href={"#"} className={"page-link"}>3</a>
+                  </li>
+                  <li className={"page-item"}>
+                    <a href={"#"} className={"page-link"}>Next</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default BlogContentSection;
--- pages/project2/blog.jsx
+++ pages/project2/blog.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import HeaderSection from "../../components/project2/HeaderSection";
+import FooterSection from "../../components/project2/FooterSection";
+import TopBannerSection from "../../components/project2/TopBannerSection";
+import BlogContentSection from "../../components/project2/BlogContentSection";
 
 const MyComponent = () => {
   return (
-    <div>
-
-    </div>
+    <>
+      <HeaderSection/>
+      <TopBannerSection/>
+      <BlogContentSection/>
+      <FooterSection/>
+    </>
   );
 };
 
